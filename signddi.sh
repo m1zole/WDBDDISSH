867d2547abe3af331839206461bf8dc914b76b1a
3zore <minrei728@gmail.com>
2023-08-02 19:12:03 +0900

    16.6

--- signddi.sh
+++ signddi.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+openssl dgst -sign key.pem -out DeveloperDiskImageModified_16.6.dmg.signature -binary -sha1 DeveloperDiskImageModified_16.6.dmg
 openssl dgst -sign key.pem -out DeveloperDiskImageModified_16.1.dmg.signature -binary -sha1 DeveloperDiskImageModified_16.1.dmg
 openssl dgst -sign key.pem -out DeveloperDiskImageModified_16.0.dmg.signature -binary -sha1 DeveloperDiskImageModified_16.0.dmg
 openssl dgst -sign key.pem -out DeveloperDiskImageModified_15.7.dmg.signature -binary -sha1 DeveloperDiskImageModified_15.7.dmg
